45b32bbf44e35045b99acae88c0dc81a17726c3c
4mjeo <4mj.eo@dsm.hs.kr>
2024-09-12 10:59:54 +0900

    project: project setting

---
+++ src/test/java/com/example/connectserver/ConnectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
