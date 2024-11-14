8a154ee6ec684e7df4c1140ff2b3f1ab9518dd96
kimsee8200 <ttungseui11@gmail.com>
2024-11-14 00:26:08 -0800

    fetch: userEntity 아이디 버그 수정

--- src/main/java/com/example/connectserver/domain/user/service/CustomOauth2UserService.java
+++ src/main/java/com/example/connectserver/domain/user/service/CustomOauth2UserService.java
@@ -4,6 +4,7 @@ import com.example.connectserver.domain.user.entity.UserEntity;
 import com.example.connectserver.global.auth.InstagramOAuthService;
 import com.example.connectserver.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequest,OAuth2User> {
@@ -23,11 +25,14 @@ public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequ
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = instagramOAuthService.loadUser(userRequest);
         Long id = Long.parseLong(Objects.requireNonNull(oAuth2User.getAttribute("id")));
+        log.info("info: "+id);
         UserEntity user = userRepository.findByInstagramId(id);
         if (user == null) {
             UserEntity userEntity = new UserEntity();
             userEntity.setInstagramId(Long.valueOf(id));
             userEntity.setInstagramUsername(oAuth2User.getAttribute("username"));
+            userEntity.setUsername(".");
+            userEntity.setIntro(".");
             userRepository.save(userEntity);
             return oAuth2User;
         }
